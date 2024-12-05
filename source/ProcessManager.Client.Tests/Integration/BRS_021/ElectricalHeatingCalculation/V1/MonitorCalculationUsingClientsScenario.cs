2dc3a031e0d735d141c2c1d0222be27fae877d0b
Dan Stenrøjl <XDAST@energinet.dk>
2024-12-05 10:35:05 +0100

    BRS 021 Electrical Heating Calculation skeleton

---
+++ source/ProcessManager.Client.Tests/Integration/BRS_021/ElectricalHeatingCalculation/V1/MonitorCalculationUsingClientsScenario.cs
@@ -0,0 +1,140 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Energinet.DataHub.Core.FunctionApp.TestCommon.FunctionAppHost;
+using Energinet.DataHub.Core.TestCommon;
+using Energinet.DataHub.ProcessManager.Abstractions.Api.Model;
+using Energinet.DataHub.ProcessManager.Abstractions.Api.Model.OrchestrationInstance;
+using Energinet.DataHub.ProcessManager.Client.Extensions.DependencyInjection;
+using Energinet.DataHub.ProcessManager.Client.Extensions.Options;
+using Energinet.DataHub.ProcessManager.Client.Tests.Fixtures;
+using Energinet.DataHub.ProcessManager.Orchestrations.Abstractions.Processes.BRS_021.ElectricalHeatingCalculation.V1.Model;
+using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit.Abstractions;
+
+namespace Energinet.DataHub.ProcessManager.Client.Tests.Integration.BRS_021.ElectricalHeatingCalculation.V1;
+
+/// <summary>
+/// Test case where we verify the Process Manager clients can be used to start a
+/// calculation orchestration (with no input parameter) and monitor its status during its lifetime.
+/// </summary>
+[Collection(nameof(ProcessManagerClientCollection))]
+public class MonitorCalculationUsingClientsScenario : IAsyncLifetime
+{
+    public MonitorCalculationUsingClientsScenario(
+        ScenarioProcessManagerAppFixture processManagerAppFixture,
+        ScenarioOrchestrationsAppFixture orchestrationsAppFixture,
+        ITestOutputHelper testOutputHelper)
+    {
+        ProcessManagerAppFixture = processManagerAppFixture;
+        ProcessManagerAppFixture.SetTestOutputHelper(testOutputHelper);
+
+        OrchestrationsAppFixture = orchestrationsAppFixture;
+        OrchestrationsAppFixture.SetTestOutputHelper(testOutputHelper);
+
+        var services = new ServiceCollection();
+        services.AddScoped(_ => CreateInMemoryConfigurations(new Dictionary<string, string?>()
+        {
+            [$"{ProcessManagerHttpClientsOptions.SectionName}:{nameof(ProcessManagerHttpClientsOptions.GeneralApiBaseAddress)}"]
+                = ProcessManagerAppFixture.AppHostManager.HttpClient.BaseAddress!.ToString(),
+            [$"{ProcessManagerHttpClientsOptions.SectionName}:{nameof(ProcessManagerHttpClientsOptions.OrchestrationsApiBaseAddress)}"]
+                = OrchestrationsAppFixture.AppHostManager.HttpClient.BaseAddress!.ToString(),
+        }));
+        services.AddProcessManagerHttpClients();
+        ServiceProvider = services.BuildServiceProvider();
+    }
+
+    private ScenarioProcessManagerAppFixture ProcessManagerAppFixture { get; }
+
+    private ScenarioOrchestrationsAppFixture OrchestrationsAppFixture { get; }
+
+    private ServiceProvider ServiceProvider { get; }
+
+    public Task InitializeAsync()
+    {
+        ProcessManagerAppFixture.AppHostManager.ClearHostLog();
+        OrchestrationsAppFixture.AppHostManager.ClearHostLog();
+
+        return Task.CompletedTask;
+    }
+
+    public async Task DisposeAsync()
+    {
+        ProcessManagerAppFixture.SetTestOutputHelper(null!);
+        OrchestrationsAppFixture.SetTestOutputHelper(null!);
+
+        await ServiceProvider.DisposeAsync();
+    }
+
+    [Fact]
+    public async Task Calculation_WhenStarted_CanMonitorLifecycle()
+    {
+        var processManagerClient = ServiceProvider.GetRequiredService<IProcessManagerClient>();
+
+        var userIdentity = new UserIdentityDto(
+            UserId: Guid.NewGuid(),
+            ActorId: Guid.NewGuid());
+
+        // Step 1: Start new calculation orchestration instance
+        var orchestrationInstanceId = await processManagerClient
+            .StartNewOrchestrationInstanceAsync(
+                new StartElectricalHeatingCalculationCommandV1(
+                    userIdentity),
+                CancellationToken.None);
+
+        // Step 2: Query until terminated with succeeded
+        var isTerminated = await Awaiter.TryWaitUntilConditionAsync(
+            async () =>
+            {
+                var orchestrationInstance = await processManagerClient
+                    .GetOrchestrationInstanceByIdAsync(
+                        new GetOrchestrationInstanceByIdQuery(
+                            userIdentity,
+                            orchestrationInstanceId),
+                        CancellationToken.None);
+
+                return
+                    orchestrationInstance.Lifecycle.State == OrchestrationInstanceLifecycleStates.Terminated
+                    && orchestrationInstance.Lifecycle.TerminationState == OrchestrationInstanceTerminationStates.Succeeded;
+            },
+            timeLimit: TimeSpan.FromSeconds(60),
+            delay: TimeSpan.FromSeconds(3));
+
+        isTerminated.Should().BeTrue("because we expects the orchestration instance can complete within given wait time");
+
+        // Step 3: General search using name and termination state
+        var orchestrationInstancesGeneralSearch = await processManagerClient
+            .SearchOrchestrationInstancesByNameAsync(
+                new SearchOrchestrationInstancesByNameQuery(
+                    userIdentity,
+                    name: new Brs_021_ElectricalHeatingCalculation_V1().Name,
+                    version: null,
+                    lifecycleState: OrchestrationInstanceLifecycleStates.Terminated,
+                    terminationState: OrchestrationInstanceTerminationStates.Succeeded,
+                    startedAtOrLater: null,
+                    terminatedAtOrEarlier: null),
+                CancellationToken.None);
+
+        orchestrationInstancesGeneralSearch.Should().Contain(x => x.Id == orchestrationInstanceId);
+    }
+
+    private IConfiguration CreateInMemoryConfigurations(Dictionary<string, string?> configurations)
+    {
+        return new ConfigurationBuilder()
+            .AddInMemoryCollection(configurations)
+            .Build();
+    }
+}

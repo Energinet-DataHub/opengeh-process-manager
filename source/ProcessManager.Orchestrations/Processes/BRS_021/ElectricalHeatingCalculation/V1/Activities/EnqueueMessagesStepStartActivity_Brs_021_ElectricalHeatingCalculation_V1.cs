8ea027f1ae677cca9fba83324e61cf12048b2a77
BjarkeMeier <BjarkeMeier@users.noreply.github.com>
2024-12-06 14:53:10 +0100

    feat: Make BRS-021 electrical heating registration more generic (#28)

--- source/ProcessManager.Orchestrations/Processes/BRS_021/ElectricalHeatingCalculation/V1/Activities/EnqueueMessagesStepStartActivity_Brs_021_ElectricalHeatingCalculation_V1.cs
+++ source/ProcessManager.Orchestrations/Processes/BRS_021/ElectricalHeatingCalculation/V1/Activities/EnqueueMessagesStepStartActivity_Brs_021_ElectricalHeatingCalculation_V1.cs
@@ -34,7 +34,7 @@ internal class EnqueueMessagesStepStartActivity_Brs_021_ElectricalHeatingCalcula
             .GetAsync(new OrchestrationInstanceId(orchestrationInstanceId))
             .ConfigureAwait(false);
 
-        var step = orchestrationInstance.Steps.Single(x => x.Sequence == Orchestration_Brs_021_ElectricalHeatingCalculation_V1.EnqueueMessagesStepSequence);
+        var step = orchestrationInstance.Steps.Single(x => x.Sequence == Orchestration_Brs_021_ElectricalHeatingCalculation_V1.EnqueueMessagesStep.Sequence);
         if (!step.IsSkipped())
         {
             step.Lifecycle.TransitionToRunning(Clock);

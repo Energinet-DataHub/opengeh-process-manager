c9c80dd177e5e87f62d65e0f06e9f1ce3c0d4017
Mads Magnus Due <madsmagnusdue@hotmail.com>
2024-12-09 21:10:08 +0100

    feat: create orchestration for brs21 metered data (#31)
    * Added initial BRS021 models and classes
    Co-authored-by: Anders Balling Petersen <abp@abpconsulting.dk>

---
+++ source/ProcessManager.Orchestrations/Processes/BRS_021/ForwardMeteredData/V1/Orchestration_Brs_021_ForwardMeteredData_V1.cs
@@ -0,0 +1,47 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Energinet.DataHub.ProcessManagement.Core.Infrastructure.Extensions.DurableTask;
+using Energinet.DataHub.ProcessManager.Orchestrations.Abstractions.Processes.BRS_021.ForwardMeteredData.V1.Model;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.DurableTask;
+
+namespace Energinet.DataHub.ProcessManager.Orchestrations.Processes.BRS_021.ForwardMeteredData.V1;
+
+internal class Orchestration_Brs_021_ForwardMeteredData_V1
+{
+    internal const int ValidatingStep = 1;
+    internal const int StoringMeteredDataStep = 2;
+    internal const int FindReceiverStep = 3;
+    internal const int EnqueueMessagesStep = 4;
+
+    [Function(nameof(Orchestration_Brs_021_ForwardMeteredData_V1))]
+    public async Task<string> Run(
+        [OrchestrationTrigger] TaskOrchestrationContext context)
+    {
+        var input = context.GetOrchestrationParameterValue<MeteredDataForMeasurementPointMessageInputV1>();
+
+        if (input == null)
+            return "Error: No input specified.";
+
+        await Task.CompletedTask;
+
+        /*
+         * Activities:
+         */
+        // TODO: Implementing activities coming in next PR.
+
+        return "Success";
+    }
+}

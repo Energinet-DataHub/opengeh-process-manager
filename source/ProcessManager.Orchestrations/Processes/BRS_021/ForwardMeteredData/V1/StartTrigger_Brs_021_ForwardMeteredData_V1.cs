da8bf013a8d7f5393389c8950180add15ff727ef
Dan Stenrøjl <XDAST@energinet.dk>
2024-12-27 11:35:46 +0100

    TODO's

--- source/ProcessManager.Orchestrations/Processes/BRS_021/ForwardMeteredData/V1/StartTrigger_Brs_021_ForwardMeteredData_V1.cs
+++ source/ProcessManager.Orchestrations/Processes/BRS_021/ForwardMeteredData/V1/StartTrigger_Brs_021_ForwardMeteredData_V1.cs
@@ -19,6 +19,11 @@ using Microsoft.Azure.Functions.Worker;
 
 namespace Energinet.DataHub.ProcessManager.Orchestrations.Processes.BRS_021.ForwardMeteredData.V1;
 
+// TODO: We have decided to route on the "name" part of the "orchestration description unique name",
+// meaning not including the "version" part; this will minimize how often we need to adjust infrastructure
+// with regards to "subscriptions". Hence this trigger should not be located within the "V1".
+// Also we need a generic way to first parse the "version" of a command and then direct the message to
+// the correct "version handler."
 public class StartTrigger_Brs_021_ForwardMeteredData_V1(
     StartForwardMeteredDataHandlerV1 handler)
 {

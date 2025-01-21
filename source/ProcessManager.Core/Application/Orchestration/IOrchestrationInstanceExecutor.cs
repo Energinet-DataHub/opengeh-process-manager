5fb21eb30f6366614b9ce22c430139e27e6f79f5
Ebbe Knudsen <xedkn@energinet.dk>
2025-01-21 10:09:51 +0100

    Removev unused usings

--- source/ProcessManager.Core/Application/Orchestration/IOrchestrationInstanceExecutor.cs
+++ source/ProcessManager.Core/Application/Orchestration/IOrchestrationInstanceExecutor.cs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using Energinet.DataHub.ProcessManager.Abstractions.Api.Model;
 using Energinet.DataHub.ProcessManager.Core.Domain.OrchestrationDescription;
 using Energinet.DataHub.ProcessManager.Core.Domain.OrchestrationInstance;
 

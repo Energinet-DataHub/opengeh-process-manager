b9a2d4a710ea3b11fbd5aee1b47e4366a6205933
Mads Magnus Due <madsmagnusdue@hotmail.com>
2025-01-21 11:04:52 +0100

    feat: updated MeteredDataForMeteringPointRejectedV1 to use DataHubTypes

--- source/ProcessManager.Orchestrations.Abstractions/Processes/BRS_021/ForwardMeteredData/V1/Model/MeteredDataForMeteringPointRejectedV1.cs
+++ source/ProcessManager.Orchestrations.Abstractions/Processes/BRS_021/ForwardMeteredData/V1/Model/MeteredDataForMeteringPointRejectedV1.cs
@@ -12,15 +12,16 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using Energinet.DataHub.ProcessManager.Orchestrations.Abstractions.Components.Datahub.ValueObjects;
+
 namespace Energinet.DataHub.ProcessManager.Orchestrations.Abstractions.Processes.BRS_021.ForwardMeteredData.V1.Model;
 
 public readonly record struct TimeInterval(DateTimeOffset Start, DateTimeOffset End);
 
 public sealed record MeteredDataForMeteringPointRejectedV1(
     string EventId,
-    string BusinessReason,
-    string ReceiverId,
-    string ReceiverRole,
+    BusinessReason BusinessReason,
+    MarketActorRecipient MarketActorRecipient,
     Guid ProcessId,
     Guid ExternalId,
     AcknowledgementV1 AcknowledgementV1);

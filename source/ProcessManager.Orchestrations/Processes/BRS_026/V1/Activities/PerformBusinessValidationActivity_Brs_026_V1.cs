9d86c761a0dc8b3e4ec66c1023066a6804773eb8
Ebbe Knudsen <xedkn@energinet.dk>
2025-01-27 11:03:46 +0100

    Set custom state

--- source/ProcessManager.Orchestrations/Processes/BRS_026/V1/Activities/PerformBusinessValidationActivity_Brs_026_V1.cs
+++ source/ProcessManager.Orchestrations/Processes/BRS_026/V1/Activities/PerformBusinessValidationActivity_Brs_026_V1.cs
@@ -12,7 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using System.Text.Json;
 using Energinet.DataHub.ProcessManager.Components.BusinessValidation;
+using Energinet.DataHub.ProcessManager.Core.Application.Orchestration;
+using Energinet.DataHub.ProcessManager.Core.Domain.OrchestrationInstance;
 using Energinet.DataHub.ProcessManager.Orchestrations.Abstractions.Processes.BRS_026.V1.Model;
 using Energinet.DataHub.ProcessManager.Orchestrations.Processes.BRS_026.V1.BusinessValidation;
 using Microsoft.Azure.Functions.Worker;
@@ -23,8 +26,10 @@ namespace Energinet.DataHub.ProcessManager.Orchestrations.Processes.BRS_026.V1.A
 /// Perform async validation (and set step to running)
 /// </summary>
 internal class PerformBusinessValidationActivity_Brs_026_V1(
+    IOrchestrationInstanceProgressRepository repository,
     BusinessValidator<RequestCalculatedEnergyTimeSeriesInputV1> validator)
 {
+    private readonly IOrchestrationInstanceProgressRepository _repository = repository;
     private readonly BusinessValidator<RequestCalculatedEnergyTimeSeriesInputV1> _validator = validator;
 
     [Function(nameof(PerformBusinessValidationActivity_Brs_026_V1))]
@@ -34,12 +39,24 @@ internal class PerformBusinessValidationActivity_Brs_026_V1(
         var validationErrors = await _validator.ValidateAsync(input.RequestInput)
             .ConfigureAwait(false);
 
-        return new ActivityOutput(
+        var activityOutput = new ActivityOutput(
             IsValid: validationErrors.Count == 0,
             ValidationErrors: validationErrors);
+
+        if (validationErrors.Count > 0)
+        {
+            var orchestrationInstance = await _repository.GetAsync(input.OrchestrationInstanceId).ConfigureAwait(false);
+            var step = orchestrationInstance.GetStep(input.StepSqeuence);
+            step.SetCustomState(JsonSerializer.Serialize(activityOutput));
+            await _repository.UnitOfWork.CommitAsync().ConfigureAwait(false);
+        }
+
+        return activityOutput;
     }
 
     public record ActivityInput(
+        OrchestrationInstanceId OrchestrationInstanceId,
+        int StepSqeuence,
         RequestCalculatedEnergyTimeSeriesInputV1 RequestInput);
 
     public record ActivityOutput(

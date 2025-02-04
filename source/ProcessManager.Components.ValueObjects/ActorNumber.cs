6d40da8c35919b2ef1c6a03305e908679155900b
Dan Stenrøjl <XDAST@energinet.dk>
2025-02-04 10:40:06 +0100

    Refactor namespace for ActorNumber

--- source/ProcessManager.Components.ValueObjects/ActorNumber.cs
+++ source/ProcessManager.Components.ValueObjects/ActorNumber.cs
@@ -14,7 +14,7 @@
 
 using System.Text.Json.Serialization;
 
-namespace Energinet.DataHub.ProcessManager.Orchestrations.Abstractions.Components.Datahub.ValueObjects;
+namespace Energinet.DataHub.ProcessManager.Components.ValueObjects;
 
 public record ActorNumber
 {

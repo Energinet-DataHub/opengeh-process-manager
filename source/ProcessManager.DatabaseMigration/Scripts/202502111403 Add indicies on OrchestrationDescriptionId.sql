271d5b52a4473774de96255ea58db494d9ba88f4
Mathias Ormstrup Bjerregaard <mwo@energinet.dk>
2025-02-11 14:12:22 +0100

    feat: Add IX on OrchestrationDescriptionId for OI and SI

---
+++ source/ProcessManager.DatabaseMigration/Scripts/202502111403 Add indicies on OrchestrationDescriptionId.sql
@@ -0,0 +1,19 @@
+﻿IF NOT EXISTS (SELECT 1
+               FROM sys.indexes
+               WHERE name = 'IX_OrchestrationInstance_OrchestrationDescriptionId'
+               AND object_id = OBJECT_ID('[pm].[OrchestrationInstance]'))
+BEGIN
+CREATE NONCLUSTERED INDEX IX_OrchestrationInstance_OrchestrationDescriptionId
+    ON [pm].[OrchestrationInstance]([OrchestrationDescriptionId]);
+END
+GO
+
+IF NOT EXISTS (SELECT 1
+               FROM sys.indexes
+               WHERE name = 'IX_StepInstance_OrchestrationDescriptionId'
+               AND object_id = OBJECT_ID('[pm].[StepInstance]'))
+BEGIN
+CREATE NONCLUSTERED INDEX IX_StepInstance_OrchestrationDescriptionId
+    ON [pm].[StepInstance]([OrchestrationDescriptionId]);
+END
+GO

349eae0d6d692a3b1187a219aa8ae68e7a9a4cbf
Ebbe Knudsen <xedkn@energinet.dk>
2025-02-12 15:05:49 +0100

    Add DB migrations

---
+++ source/ProcessManager.DatabaseMigration/Scripts/202502121450 Add lifecycle ActorNumber and ActorRole to OrchestrationInstance.sql
@@ -0,0 +1,6 @@
+﻿ALTER TABLE [pm].[OrchestrationInstance]
+    ADD [Lifecycle_CreatedBy_ActorNumber]   NVARCHAR(16) NULL,
+        [Lifecycle_CreatedBy_ActorRole]     NVARCHAR(50) NULL,
+        [Lifecycle_CanceledBy_ActorNumber]  NVARCHAR(16) NULL,
+        [Lifecycle_CanceledBy_ActorRole]    NVARCHAR(50) NULL
+GO
---
+++ source/ProcessManager.DatabaseMigration/Scripts/202502121451 Set created by default values for lifecycle ActorNumber and ActorRole on OrchestrationInstance.sql
@@ -0,0 +1,5 @@
+﻿UPDATE [pm].[OrchestrationInstance]
+    SET [Lifecycle_CreatedBy_ActorNumber] = '0000000000000001',
+        [Lifecycle_CreatedBy_ActorRole]   = 'SystemOperator'
+    WHERE [Lifecycle_CreatedBy_ActorId] IS NULL
+GO

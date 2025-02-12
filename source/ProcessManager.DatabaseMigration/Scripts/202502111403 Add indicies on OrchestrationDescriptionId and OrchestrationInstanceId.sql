47850c5dac7a2bcaf7f56c963737e4cf24e5cf6c
Mathias Ormstrup Bjerregaard <152613120+MWO1024@users.noreply.github.com>
2025-02-12 07:45:46 +0100

    feat: Add IX on FKs for the Orchstration- and StepInstance tables (#177)

---
+++ source/ProcessManager.DatabaseMigration/Scripts/202502111403 Add indicies on OrchestrationDescriptionId and OrchestrationInstanceId.sql
@@ -0,0 +1,19 @@
+﻿IF NOT EXISTS (SELECT 1
+               FROM sys.indexes
+               WHERE name = 'IX_OrchestrationInstance_OrchestrationDescriptionId'
+               AND object_id = OBJECT_ID('[pm].[OrchestrationInstance]'))
+BEGIN
+CREATE NONCLUSTERED INDEX IX_OrchestrationInstance_OrchestrationDescriptionId
+    ON [pm].[OrchestrationInstance]([OrchestrationDescriptionId]);
+END
+GO
+
+IF NOT EXISTS (SELECT 1
+               FROM sys.indexes
+               WHERE name = 'IX_StepInstance_OrchestrationInstanceId'
+               AND object_id = OBJECT_ID('[pm].[StepInstance]'))
+BEGIN
+CREATE NONCLUSTERED INDEX IX_StepInstance_OrchestrationInstanceId
+    ON [pm].[StepInstance]([OrchestrationInstanceId]);
+END
+GO

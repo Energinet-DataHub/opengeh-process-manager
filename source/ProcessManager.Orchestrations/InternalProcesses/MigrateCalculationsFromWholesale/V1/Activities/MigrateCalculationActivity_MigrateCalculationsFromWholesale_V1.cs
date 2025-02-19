c9684d324f52ea145a2d65018d95b6f87ecbf081
Dan Stenrøjl <XDAST@energinet.dk>
2025-02-19 16:18:17 +0100

    Factory

--- source/ProcessManager.Orchestrations/InternalProcesses/MigrateCalculationsFromWholesale/V1/Activities/MigrateCalculationActivity_MigrateCalculationsFromWholesale_V1.cs
+++ source/ProcessManager.Orchestrations/InternalProcesses/MigrateCalculationsFromWholesale/V1/Activities/MigrateCalculationActivity_MigrateCalculationsFromWholesale_V1.cs
@@ -12,14 +12,25 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using Energinet.DataHub.ProcessManager.Core.Application.Orchestration;
 using Energinet.DataHub.ProcessManager.Core.Domain.OrchestrationInstance;
+using Energinet.DataHub.ProcessManager.Core.Infrastructure.Database;
+using Energinet.DataHub.ProcessManager.Orchestrations.InternalProcesses.MigrateCalculationsFromWholesale.Wholesale;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.EntityFrameworkCore;
 
 namespace Energinet.DataHub.ProcessManager.Orchestrations.InternalProcesses.MigrateCalculationsFromWholesale.V1.Activities;
 
-public class MigrateCalculationActivity_MigrateCalculationsFromWholesale_V1
+public class MigrateCalculationActivity_MigrateCalculationsFromWholesale_V1(
+    WholesaleContext wholesaleContext,
+    ProcessManagerContext processManagerContext,
+    IOrchestrationInstanceFactory orchestrationInstanceFactory)
 {
     public const string MigratedWholesaleCalculationIdCustomStatePrefix = "MigratedWholesaleCalculationId=";
+
+    private readonly WholesaleContext _wholesaleContext = wholesaleContext;
+    private readonly ProcessManagerContext _processManagerContext = processManagerContext;
+    private readonly IOrchestrationInstanceFactory _orchestrationInstanceFactory = orchestrationInstanceFactory;
 
     public static OrchestrationInstanceCustomState GetMigratedWholesaleCalculationIdCustomState(Guid wholesaleCalculationId)
     {
@@ -38,8 +49,21 @@ public class MigrateCalculationActivity_MigrateCalculationsFromWholesale_V1
     public async Task<string> Run(
         [ActivityTrigger] ActivityInput input)
     {
-        // TODO: Implement migration
-        await Task.CompletedTask.ConfigureAwait(false);
+        var migratedCalculation = await _processManagerContext.OrchestrationInstances
+            .AsNoTracking()
+            .Where(x => x.CustomState == GetMigratedWholesaleCalculationIdCustomState(input.CalculationToMigrateId))
+            .FirstOrDefaultAsync()
+            .ConfigureAwait(false);
+
+        if (migratedCalculation == null)
+        {
+            var calculation = _wholesaleContext.Calculations
+                .FirstAsync(x => x.Id == input.CalculationToMigrateId);
+
+            var orchestrationInstance = _orchestrationInstanceFactory.CreateEntity();
+            await _processManagerContext.OrchestrationInstances.AddAsync(orchestrationInstance).ConfigureAwait(false);
+            await _processManagerContext.CommitAsync().ConfigureAwait(false);
+        }
 
         return $"Migrated {input.CalculationToMigrateId}";
     }

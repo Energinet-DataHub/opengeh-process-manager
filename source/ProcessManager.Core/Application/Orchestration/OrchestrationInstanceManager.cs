1744aa768defc85657ecf01a211ad10b6340329e
Mathias Ormstrup Bjerregaard <mwo@energinet.dk>
2025-02-25 13:27:11 +0100

    misc

--- source/ProcessManager.Core/Application/Orchestration/OrchestrationInstanceManager.cs
+++ source/ProcessManager.Core/Application/Orchestration/OrchestrationInstanceManager.cs
@@ -208,7 +208,7 @@ internal class OrchestrationInstanceManager(
 
         if (orchestrationInstanceToNotify is null)
         {
-            if (_featureFlagManager.IsEnabledAsync(FeatureFlag.SilentMode).Result)
+            if (await _featureFlagManager.IsEnabledAsync(FeatureFlag.SilentMode).ConfigureAwait(false))
             {
                 _logger.LogWarning(
                     $"Notifying orchestration instance with id '{id.Value}' and event name '{eventName}' failed.");

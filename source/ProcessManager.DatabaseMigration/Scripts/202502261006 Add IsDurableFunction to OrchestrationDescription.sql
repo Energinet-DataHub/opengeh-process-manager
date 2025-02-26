815a73b3fe4bce21310715cc96d7cfe0274ca8be
Mathias Ormstrup Bjerregaard <mwo@energinet.dk>
2025-02-26 12:17:11 +0100

    fixup! feat: Add `IsDurableFunction` to `OrchestrationDescription`

--- source/ProcessManager.DatabaseMigration/Scripts/202502261006 Add IsDurableFunction to OrchestrationDescription.sql
+++ source/ProcessManager.DatabaseMigration/Scripts/202502261006 Add IsDurableFunction to OrchestrationDescription.sql
@@ -2,6 +2,14 @@
                WHERE Name = N'IsDurableFunction' AND Object_ID = Object_ID(N'[pm].[OrchestrationDescription]'))
 BEGIN
     ALTER TABLE [pm].[OrchestrationDescription]
-        ADD IsDurableFunction BIT NOT NULL DEFAULT 1;
+        ADD IsDurableFunction BIT NOT NULL CONSTRAINT D_OrchestrationDescription_IsDurableFunction DEFAULT 1;
 END
+GO
+
+ALTER TABLE [pm].[OrchestrationDescription]
+    ALTER COLUMN IsDurableFunction DROP DEFAULT;
+GO
+
+ALTER TABLE [pm].[OrchestrationDescription]
+    DROP CONSTRAINT D_OrchestrationDescription_IsDurableFunction;
 GO

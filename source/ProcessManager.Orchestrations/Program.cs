78ef88c7811c8c9e064c2cf826537b7fe8e052d2
Ebbe Knudsen <xedkn@energinet.dk>
2025-02-28 14:28:39 +0100

    Update Program.cs

--- source/ProcessManager.Orchestrations/Program.cs
+++ source/ProcessManager.Orchestrations/Program.cs
@@ -30,7 +30,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
-    .ConfigureFunctionsWebApplication((context, builder) =>
+    .ConfigureFunctionsWebApplication(builder =>
     {
         // Http => Authorization
         builder.UseFunctionsAuthorization();
@@ -41,9 +41,6 @@ var host = new HostBuilder()
             (settings, _) =>
             {
                 settings.Mode = DfmMode.ReadOnly;
-
-                if (context.HostingEnvironment.IsDevelopment())
-                    settings.DisableAuthentication = true;
             });
     })
     .ConfigureServices((context, services) =>

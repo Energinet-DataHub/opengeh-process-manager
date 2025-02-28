efdb71b18cc07a9275a33084156fd9c17fe332b1
Dan Stenrøjl <XDAST@energinet.dk>
2025-02-28 11:15:52 +0100

    Remove comment

--- source/ProcessManager.Orchestrations/Processes/BRS_021/ElectricalHeatingCalculation/V1/Orchestration_Brs_021_ElectricalHeatingCalculation_V1.cs
+++ source/ProcessManager.Orchestrations/Processes/BRS_021/ElectricalHeatingCalculation/V1/Orchestration_Brs_021_ElectricalHeatingCalculation_V1.cs
@@ -24,7 +24,6 @@ using Microsoft.DurableTask;
 
 namespace Energinet.DataHub.ProcessManager.Orchestrations.Processes.BRS_021.ElectricalHeatingCalculation.V1;
 
-// TODO: Implement according to guidelines: https://energinet.atlassian.net/wiki/spaces/D3/pages/824803345/Durable+Functions+Development+Guidelines
 internal class Orchestration_Brs_021_ElectricalHeatingCalculation_V1
 {
     public static readonly OrchestrationDescriptionUniqueNameDto UniqueName = Brs_021_ElectricalHeatingCalculation.V1;

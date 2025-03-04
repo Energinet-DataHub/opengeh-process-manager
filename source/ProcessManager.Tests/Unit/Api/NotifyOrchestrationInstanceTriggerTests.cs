3a39ac2354aed92b5b02b9dd2ebb9f106b27f22b
Dan Stenrøjl <XDAST@energinet.dk>
2025-03-04 16:00:11 +0100

    Refactor test

--- source/ProcessManager.Tests/Unit/Api/NotifyOrchestrationInstanceTriggerTests.cs
+++ source/ProcessManager.Tests/Unit/Api/NotifyOrchestrationInstanceTriggerTests.cs
@@ -19,7 +19,6 @@ using Energinet.DataHub.ProcessManager.Abstractions.Contracts;
 using Energinet.DataHub.ProcessManager.Api;
 using Energinet.DataHub.ProcessManager.Core.Application.Orchestration;
 using Energinet.DataHub.ProcessManager.Core.Domain.OrchestrationInstance;
-using Energinet.DataHub.ProcessManager.Shared.Extensions;
 using FluentAssertions;
 using Google.Protobuf;
 using Moq;
@@ -66,7 +65,7 @@ public class NotifyOrchestrationInstanceTriggerTests
                     raisedNotifyEventData = data;
                 });
 
-        var notifyOrchestrationInstanceTrigger = new NotifyOrchestrationInstanceTriggerObsolete(notifyCommandsMock.Object);
+        var notifyOrchestrationInstanceTrigger = new NotifyOrchestrationInstanceTrigger(notifyCommandsMock.Object);
 
         // When triggered
         await notifyOrchestrationInstanceTrigger.Run(receivedServiceBusMessage);

dca2f147d04c7d40380491bf7e4a9f4e28d16608
Dan Stenrøjl <XDAST@energinet.dk>
2025-03-28 13:52:27 +0100

    CalculationsQueryV1ExtensionsTests

--- source/ProcessManager.Orchestrations.Tests/Unit/CustomQueries/Calculations/V1/CalculationsQueryV1ExtensionsTests.cs
+++ source/ProcessManager.Orchestrations.Tests/Unit/CustomQueries/Calculations/V1/CalculationsQueryV1ExtensionsTests.cs
@@ -12,8 +12,209 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using Energinet.DataHub.ProcessManager.Abstractions.Api.Model.OrchestrationInstance;
+using Energinet.DataHub.ProcessManager.Abstractions.Core.ValueObjects;
+using Energinet.DataHub.ProcessManager.Orchestrations.Abstractions.CustomQueries.Calculations.V1.Model;
+using Energinet.DataHub.ProcessManager.Orchestrations.Abstractions.Processes.BRS_021.CapacitySettlementCalculation;
+using Energinet.DataHub.ProcessManager.Orchestrations.Abstractions.Processes.BRS_021.ElectricalHeatingCalculation;
+using Energinet.DataHub.ProcessManager.Orchestrations.Abstractions.Processes.BRS_021.NetConsumptionCalculation;
+using Energinet.DataHub.ProcessManager.Orchestrations.Abstractions.Processes.BRS_023_027;
+using Energinet.DataHub.ProcessManager.Orchestrations.CustomQueries.Calculations.V1;
+using FluentAssertions;
+
 namespace Energinet.DataHub.ProcessManager.Orchestrations.Tests.Unit.CustomQueries.Calculations.V1;
 
 public class CalculationsQueryV1ExtensionsTests
 {
+    private readonly UserIdentityDto _userIdentity = new(
+        UserId: Guid.NewGuid(),
+        ActorNumber: ActorNumber.Create("1111111111111"),
+        ActorRole: ActorRole.DataHubAdministrator);
+
+    [Fact]
+    public void Given_AnyOneWholesaleCalculationType_When_GetOrchestrationDescriptionNames_Then_ReturnsBrs_023_027()
+    {
+        // Given
+        var calculationQuery = new CalculationsQueryV1(_userIdentity)
+        {
+            CalculationTypes = [GetRandomWholesaleCalculationType()],
+        };
+
+        // When
+        var actual = calculationQuery.GetOrchestrationDescriptionNames();
+
+        // Then
+        actual.Should()
+            .OnlyContain(item => item == Brs_023_027.Name);
+    }
+
+    [Fact]
+    public void Given_AllWholesaleCalculationTypes_When_GetOrchestrationDescriptionNames_Then_ReturnsBrs_023_027()
+    {
+        // Given
+        var calculationQuery = new CalculationsQueryV1(_userIdentity)
+        {
+            CalculationTypes = [
+                CalculationTypeQueryParameterV1.BalanceFixing,
+                CalculationTypeQueryParameterV1.Aggregation,
+                CalculationTypeQueryParameterV1.WholesaleFixing,
+                CalculationTypeQueryParameterV1.FirstCorrectionSettlement,
+                CalculationTypeQueryParameterV1.SecondCorrectionSettlement,
+                CalculationTypeQueryParameterV1.ThirdCorrectionSettlement],
+        };
+
+        // When
+        var actual = calculationQuery.GetOrchestrationDescriptionNames();
+
+        // Then
+        actual.Should()
+            .OnlyContain(item => item == Brs_023_027.Name);
+    }
+
+    [Fact]
+    public void Given_AllCalculationTypes_When_GetOrchestrationDescriptionNames_Then_ReturnsAllOrchestrationDescriptionNames()
+    {
+        // Given
+        var calculationQuery = new CalculationsQueryV1(_userIdentity)
+        {
+            CalculationTypes = GetAllCalculationTypes(),
+        };
+
+        // When
+        var actual = calculationQuery.GetOrchestrationDescriptionNames();
+
+        // Then
+        actual.Should()
+            .Contain([
+                Brs_021_ElectricalHeatingCalculation.Name,
+                Brs_021_CapacitySettlementCalculation.Name,
+                Brs_021_NetConsumptionCalculation.Name,
+                Brs_023_027.Name]);
+    }
+
+    [Fact]
+    public void Given_AllButWholesaleCalculationTypes_When_GetOrchestrationDescriptionNames_Then_ReturnsAllOrchestrationDescriptionNamesExceptBrs_023_027()
+    {
+        // Given
+        var calculationQuery = new CalculationsQueryV1(_userIdentity)
+        {
+            CalculationTypes = [
+                CalculationTypeQueryParameterV1.ElectricalHeating,
+                CalculationTypeQueryParameterV1.CapacitySettlement,
+                CalculationTypeQueryParameterV1.NetConsumption],
+        };
+
+        // When
+        var actual = calculationQuery.GetOrchestrationDescriptionNames();
+
+        // Then
+        actual.Should()
+            .Contain([
+                Brs_021_ElectricalHeatingCalculation.Name,
+                Brs_021_CapacitySettlementCalculation.Name,
+                Brs_021_NetConsumptionCalculation.Name])
+            .And.NotContain([
+                Brs_023_027.Name]);
+    }
+
+    [Fact]
+    public void Given_AllCalculationTypesAndIsInternalCalculation_When_GetOrchestrationDescriptionNames_Then_ReturnsOnlyBrs_023_027()
+    {
+        // Given
+        var calculationQuery = new CalculationsQueryV1(_userIdentity)
+        {
+            CalculationTypes = GetAllCalculationTypes(),
+            IsInternalCalculation = true,
+        };
+
+        // When
+        var actual = calculationQuery.GetOrchestrationDescriptionNames();
+
+        // Then
+        actual.Should()
+            .OnlyContain(item => item == Brs_023_027.Name);
+    }
+
+    [Fact]
+    public void Given_AllCalculationTypesAndGridAreaCodesIsSpecified_When_GetOrchestrationDescriptionNames_Then_ReturnsOnlyBrs_023_027()
+    {
+        // Given
+        var calculationQuery = new CalculationsQueryV1(_userIdentity)
+        {
+            CalculationTypes = GetAllCalculationTypes(),
+            GridAreaCodes = ["804"],
+        };
+
+        // When
+        var actual = calculationQuery.GetOrchestrationDescriptionNames();
+
+        // Then
+        actual.Should()
+            .OnlyContain(item => item == Brs_023_027.Name);
+    }
+
+    [Fact]
+    public void Given_AllCalculationTypesAndPeriodStartIsSpecified_When_GetOrchestrationDescriptionNames_Then_ReturnsOnlyBrs_023_027AndBrs_021_CapacitySettlement()
+    {
+        // Given
+        var calculationQuery = new CalculationsQueryV1(_userIdentity)
+        {
+            CalculationTypes = GetAllCalculationTypes(),
+            PeriodStartDate = new DateTimeOffset(2025, 02, 19, 23, 00, 00, TimeSpan.Zero),
+        };
+
+        // When
+        var actual = calculationQuery.GetOrchestrationDescriptionNames();
+
+        // Then
+        actual.Should()
+            .Contain([
+                Brs_021_CapacitySettlementCalculation.Name,
+                Brs_023_027.Name])
+            .And.NotContain([
+                Brs_021_ElectricalHeatingCalculation.Name,
+                Brs_021_NetConsumptionCalculation.Name]);
+    }
+
+    [Fact]
+    public void Given_AllCalculationTypesAndPeriodEndIsSpecified_When_GetOrchestrationDescriptionNames_Then_ReturnsOnlyBrs_023_027AndBrs_021_CapacitySettlement()
+    {
+        // Given
+        var calculationQuery = new CalculationsQueryV1(_userIdentity)
+        {
+            CalculationTypes = GetAllCalculationTypes(),
+            PeriodEndDate = new DateTimeOffset(2025, 02, 20, 23, 00, 00, TimeSpan.Zero),
+        };
+
+        // When
+        var actual = calculationQuery.GetOrchestrationDescriptionNames();
+
+        // Then
+        actual.Should()
+            .Contain([
+                Brs_021_CapacitySettlementCalculation.Name,
+                Brs_023_027.Name])
+            .And.NotContain([
+                Brs_021_ElectricalHeatingCalculation.Name,
+                Brs_021_NetConsumptionCalculation.Name]);
+    }
+
+    private static CalculationTypeQueryParameterV1 GetRandomWholesaleCalculationType()
+    {
+        return (CalculationTypeQueryParameterV1)new Random().Next(0, 6);
+    }
+
+    private static IReadOnlyCollection<CalculationTypeQueryParameterV1> GetAllCalculationTypes()
+    {
+        return [
+            CalculationTypeQueryParameterV1.BalanceFixing,
+            CalculationTypeQueryParameterV1.Aggregation,
+            CalculationTypeQueryParameterV1.WholesaleFixing,
+            CalculationTypeQueryParameterV1.FirstCorrectionSettlement,
+            CalculationTypeQueryParameterV1.SecondCorrectionSettlement,
+            CalculationTypeQueryParameterV1.ThirdCorrectionSettlement,
+            CalculationTypeQueryParameterV1.ElectricalHeating,
+            CalculationTypeQueryParameterV1.CapacitySettlement,
+            CalculationTypeQueryParameterV1.NetConsumption];
+    }
 }

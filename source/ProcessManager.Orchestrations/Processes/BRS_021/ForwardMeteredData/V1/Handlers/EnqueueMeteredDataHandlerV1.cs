832c52f6e77671d5c7ac45d5367d628b2bddebe8
Mathias Ormstrup Bjerregaard <152613120+MWO1024@users.noreply.github.com>
2025-03-31 12:38:47 +0200

    fix: Remove `OriginalTransactionId` from `ForwardMeteredDataAcceptedV1` (#299)
    Co-authored-by: RasmusGraabaek <rasmus.graabaek@hotmail.com>

--- source/ProcessManager.Orchestrations/Processes/BRS_021/ForwardMeteredData/V1/Handlers/EnqueueMeteredDataHandlerV1.cs
+++ source/ProcessManager.Orchestrations/Processes/BRS_021/ForwardMeteredData/V1/Handlers/EnqueueMeteredDataHandlerV1.cs
@@ -175,7 +175,6 @@ public class EnqueueMeteredDataHandlerV1(
             OriginalActorMessageId: forwardMeteredDataInput.ActorMessageId,
             MeteringPointId: forwardMeteredDataInput.MeteringPointId,
             MeteringPointType: MeteringPointType.FromName(forwardMeteredDataInput.MeteringPointType),
-            OriginalTransactionId: forwardMeteredDataInput.TransactionId,
             ProductNumber: forwardMeteredDataInput.ProductNumber,
             RegistrationDateTime: InstantPatternWithOptionalSeconds.Parse(forwardMeteredDataInput.RegistrationDateTime).Value.ToDateTimeOffset(),
             StartDateTime: InstantPatternWithOptionalSeconds.Parse(forwardMeteredDataInput.StartDateTime).Value.ToDateTimeOffset(),

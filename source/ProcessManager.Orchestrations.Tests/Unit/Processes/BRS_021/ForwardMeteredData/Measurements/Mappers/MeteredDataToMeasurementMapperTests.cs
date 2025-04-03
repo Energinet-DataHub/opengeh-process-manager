9b1525f1105d96643ccec6b6257675f71547690d
Alexander <alexanderharrow@hotmail.com>
2025-04-03 15:11:47 +0200

    Updates

--- source/ProcessManager.Orchestrations.Tests/Unit/Processes/BRS_021/ForwardMeteredData/Measurements/Mappers/MeteredDataToMeasurementMapperTests.cs
+++ source/ProcessManager.Orchestrations.Tests/Unit/Processes/BRS_021/ForwardMeteredData/Measurements/Mappers/MeteredDataToMeasurementMapperTests.cs
@@ -153,10 +153,6 @@ public class MeteredDataToMeasurementMapperTests
                     PMTypes.MeteringPointType.CollectiveNetConsumption,
                     MeasurementsTypes.MeteringPointType.MptCollectiveNetConsumption
                 },
-                {
-                    PMTypes.MeteringPointType.InternalUse,
-                    MeasurementsTypes.MeteringPointType.MptInternalUse
-                },
             };
 
         MeteredDataToMeasurementMapper.MeteringPointType.Should().BeEquivalentTo(expectedMappings);

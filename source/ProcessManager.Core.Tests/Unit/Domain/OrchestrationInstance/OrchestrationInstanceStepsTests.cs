ba47226d4cd0f5545dd7cb0e99f51b8cf4effcf4
Dan Stenrøjl <XDAST@energinet.dk>
2025-04-10 17:12:40 +0200

    OrchestrationInstanceStepsTests

--- source/ProcessManager.Core.Tests/Unit/Domain/OrchestrationInstance/OrchestrationInstanceStepsTests.cs
+++ source/ProcessManager.Core.Tests/Unit/Domain/OrchestrationInstance/OrchestrationInstanceStepsTests.cs
@@ -12,12 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using Energinet.DataHub.ProcessManager.Core.Domain.OrchestrationDescription;
-using Energinet.DataHub.ProcessManager.Core.Domain.OrchestrationInstance;
 using Energinet.DataHub.ProcessManager.Shared.Tests.Fixtures;
 using FluentAssertions;
-using NodaTime;
-using CoreDomain = Energinet.DataHub.ProcessManager.Core.Domain;
 
 namespace Energinet.DataHub.ProcessManager.Core.Tests.Unit.Domain.OrchestrationInstance;
 
@@ -26,8 +22,8 @@ public class OrchestrationInstanceStepsTests
     [Fact]
     public void ExistingStepSequence_WhenGetStep_StepWithSequenceIsReturned()
     {
-        var orchestrationDescription = CreateOrchestrationDescription();
-        var instance = CreateOrchestrationInstance(orchestrationDescription);
+        var orchestrationDescription = DomainTestDataFactory.CreateOrchestrationDescription();
+        var instance = DomainTestDataFactory.CreateUserInitiatedOrchestrationInstance(orchestrationDescription);
         const int stepSequence = 1;
 
         // Act
@@ -39,8 +35,8 @@ public class OrchestrationInstanceStepsTests
     [Fact]
     public void ExistingStepSequence_WhenTryGetStep_StepWithSequenceIsReturned()
     {
-        var orchestrationDescription = CreateOrchestrationDescription();
-        var instance = CreateOrchestrationInstance(orchestrationDescription);
+        var orchestrationDescription = DomainTestDataFactory.CreateOrchestrationDescription();
+        var instance = DomainTestDataFactory.CreateUserInitiatedOrchestrationInstance(orchestrationDescription);
         const int stepSequence = 1;
 
         // Act
@@ -54,8 +50,8 @@ public class OrchestrationInstanceStepsTests
     [Fact]
     public void UnknownStepSequence_WhenTryGetStep_ReturnsNoStep()
     {
-        var orchestrationDescription = CreateOrchestrationDescription();
-        var instance = CreateOrchestrationInstance(orchestrationDescription);
+        var orchestrationDescription = DomainTestDataFactory.CreateOrchestrationDescription();
+        var instance = DomainTestDataFactory.CreateUserInitiatedOrchestrationInstance(orchestrationDescription);
         const int stepSequence = -1;
 
         // Act
@@ -68,61 +64,13 @@ public class OrchestrationInstanceStepsTests
     [Fact]
     public void UnknownStepSequence_WhenGetStep_ThrowsArgumentOfOfRangeException()
     {
-        var orchestrationDescription = CreateOrchestrationDescription();
-        var instance = CreateOrchestrationInstance(orchestrationDescription);
+        var orchestrationDescription = DomainTestDataFactory.CreateOrchestrationDescription();
+        var instance = DomainTestDataFactory.CreateUserInitiatedOrchestrationInstance(orchestrationDescription);
         const int stepSequence = -1;
 
         // Act
         instance.Invoking(i => i.GetStep(stepSequence))
             .Should()
             .Throw<ArgumentOutOfRangeException>();
-    }
-
-    private static CoreDomain.OrchestrationInstance.OrchestrationInstance CreateOrchestrationInstance(
-        CoreDomain.OrchestrationDescription.OrchestrationDescription orchestrationDescription,
-        Instant? runAt = default)
-    {
-        var userIdentity = DomainTestDataFactory.EnergySupplier.UserIdentity;
-
-        var orchestrationInstance = CoreDomain.OrchestrationInstance.OrchestrationInstance.CreateFromDescription(
-            userIdentity,
-            orchestrationDescription,
-            skipStepsBySequence: [],
-            clock: SystemClock.Instance,
-            runAt: runAt,
-            actorMessageId: new ActorMessageId(Guid.NewGuid().ToString()),
-            transactionId: new TransactionId(Guid.NewGuid().ToString()),
-            meteringPointId: new MeteringPointId(Guid.NewGuid().ToString()));
-
-        orchestrationInstance.ParameterValue.SetFromInstance(new TestOrchestrationParameter
-        {
-            TestString = "Test string",
-            TestInt = 42,
-        });
-
-        return orchestrationInstance;
-    }
-
-    private static CoreDomain.OrchestrationDescription.OrchestrationDescription CreateOrchestrationDescription(OrchestrationDescriptionUniqueName? uniqueName = default)
-    {
-        var orchestrationDescription = new CoreDomain.OrchestrationDescription.OrchestrationDescription(
-            uniqueName: uniqueName ?? new OrchestrationDescriptionUniqueName("TestOrchestration", 4),
-            canBeScheduled: true,
-            functionName: "TestOrchestrationFunction");
-
-        orchestrationDescription.ParameterDefinition.SetFromType<TestOrchestrationParameter>();
-
-        orchestrationDescription.AppendStepDescription("Test step 1");
-        orchestrationDescription.AppendStepDescription("Test step 2");
-        orchestrationDescription.AppendStepDescription("Test step 3");
-
-        return orchestrationDescription;
-    }
-
-    private class TestOrchestrationParameter
-    {
-        public string? TestString { get; set; }
-
-        public int? TestInt { get; set; }
     }
 }

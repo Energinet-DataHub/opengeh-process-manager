59b2b4a99975a01056d8eba81ce1a14986be9a93
Dan Stenrøjl <XDAST@energinet.dk>
2025-04-24 18:58:31 +0200

    Test data

--- source/ProcessManager.Orchestrations.Tests/Integration/Processes/BRS_021/ElectricalHeatingCalculation/Databricks/SqlStatements/CalculatedMeasurementsQueryTests.cs
+++ source/ProcessManager.Orchestrations.Tests/Integration/Processes/BRS_021/ElectricalHeatingCalculation/Databricks/SqlStatements/CalculatedMeasurementsQueryTests.cs
@@ -45,11 +45,11 @@ public class CalculatedMeasurementsQueryTests : IClassFixture<CalculatedMeasurem
             sut.DataObjectName,
             [
                 // First transaction
-                ["'capacity_settlement'", "'48362f74-37e0-4330-b071-b64d0d564b9c'", "'1a0c19a9-8310-5e59-b2e0-d1533927c6b9'", "'2025-04-07T10:04:55.692'", "'190000040000000001'", "'capacity_settlement'", "'2025-01-14T22:00:00.000'", "'0.000'", "'kWh'", "'calculated'", "NULL", "'PT1H'"],
-                ["'capacity_settlement'", "'48362f74-37e0-4330-b071-b64d0d564b9c'", "'1a0c19a9-8310-5e59-b2e0-d1533927c6b9'", "'2025-04-07T10:04:55.692'", "'190000040000000001'", "'capacity_settlement'", "'2025-01-14T23:00:00.000'", "'4.739'", "'kWh'", "'calculated'", "NULL", "'PT1H'"],
+                ["'capacity_settlement'", "'48362f74-37e0-4330-b071-b64d0d564b9c'", "'1a0c19a9-8310-5e59-b2e0-d1533927c6b9'", "'2025-04-07T10:04:55.692'", "'190000040000000001'", "'capacity_settlement'", "'2025-01-14T22:00:00.000'", "0.000", "'kWh'", "'calculated'", "'PT1H'"],
+                ["'capacity_settlement'", "'48362f74-37e0-4330-b071-b64d0d564b9c'", "'1a0c19a9-8310-5e59-b2e0-d1533927c6b9'", "'2025-04-07T10:04:55.692'", "'190000040000000001'", "'capacity_settlement'", "'2025-01-14T23:00:00.000'", "4.739", "'kWh'", "'calculated'", "'PT1H'"],
                 // Second transaction
-                ["'capacity_settlement'", "'48362f74-37e0-4330-b071-b64d0d564b9c'", "'1a790ec1-e1d8-51ed-84fd-15d37ad5021a'", "'2025-04-07T10:04:55.692'", "'190000040000000001'", "'capacity_settlement'", "'2025-01-29T22:00:00.000'", "'0.000'", "'kWh'", "'calculated'", "NULL", "'PT1H'"],
-                ["'capacity_settlement'", "'48362f74-37e0-4330-b071-b64d0d564b9c'", "'1a790ec1-e1d8-51ed-84fd-15d37ad5021a'", "'2025-04-07T10:04:55.692'", "'190000040000000001'", "'capacity_settlement'", "'2025-01-29T23:00:00.000'", "'0.000'", "'kWh'", "'calculated'", "NULL", "'PT1H'"],
+                ["'capacity_settlement'", "'48362f74-37e0-4330-b071-b64d0d564b9c'", "'1a790ec1-e1d8-51ed-84fd-15d37ad5021a'", "'2025-04-07T10:04:55.692'", "'190000040000000001'", "'capacity_settlement'", "'2025-01-29T22:00:00.000'", "0.000", "'kWh'", "'calculated'", "'PT1H'"],
+                ["'capacity_settlement'", "'48362f74-37e0-4330-b071-b64d0d564b9c'", "'1a790ec1-e1d8-51ed-84fd-15d37ad5021a'", "'2025-04-07T10:04:55.692'", "'190000040000000001'", "'capacity_settlement'", "'2025-01-29T23:00:00.000'", "4.739", "'kWh'", "'calculated'", "'PT1H'"],
             ]);
     }
 }

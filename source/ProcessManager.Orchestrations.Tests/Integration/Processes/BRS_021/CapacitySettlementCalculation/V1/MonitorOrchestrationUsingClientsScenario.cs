4fc431553eef42cbd791c3e503b5f6b37c58af19
Alexander <alexanderharrow@hotmail.com>
2025-04-30 11:05:27 +0200

    Updates

--- source/ProcessManager.Orchestrations.Tests/Integration/Processes/BRS_021/CapacitySettlementCalculation/V1/MonitorOrchestrationUsingClientsScenario.cs
+++ source/ProcessManager.Orchestrations.Tests/Integration/Processes/BRS_021/CapacitySettlementCalculation/V1/MonitorOrchestrationUsingClientsScenario.cs
@@ -89,7 +89,7 @@ public class MonitorOrchestrationUsingClientsScenario : IAsyncLifetime
     }
 
     [Fact]
-    public async Task Given_StartElectricalHeatingCalculationCommand_When_Started_Then_OrchestrationInstanceTerminatesWithSuccess()
+    public async Task Given_StartCapacitySettlementCalculationCommand_When_Started_Then_OrchestrationInstanceTerminatesWithSuccess()
     {
         // Mocking the databricks jobs api, forcing it to return a terminated successful job status
         Fixture.OrchestrationsAppManager.MockServer.MockDatabricksJobStatusResponse(

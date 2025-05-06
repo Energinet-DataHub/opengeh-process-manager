d2d6954fce9ea65264f5e2a59a2317ee9d251c98
Lasse Ringgren Nielsen <sixxth@gmail.com>
2025-05-06 11:19:34 +0200

    removed unused OriginalMessageId prop

--- source/ProcessManager.Orchestrations.Abstractions/Processes/BRS_026_028/BRS_028/V1/Model/RequestCalculatedWholesaleServicesRejectedV1.cs
+++ source/ProcessManager.Orchestrations.Abstractions/Processes/BRS_026_028/BRS_028/V1/Model/RequestCalculatedWholesaleServicesRejectedV1.cs
@@ -23,7 +23,6 @@ namespace Energinet.DataHub.ProcessManager.Orchestrations.Abstractions.Processes
 /// A model containing the validation errors if a wholesale services request is rejected.
 /// </summary>
 public record RequestCalculatedWholesaleServicesRejectedV1(
-    [property: Obsolete] string OriginalMessageId, // TODO: Remove after EDI is updated to use OriginalActorMessageId
     string OriginalActorMessageId,
     string OriginalTransactionId,
     ActorNumber RequestedForActorNumber,

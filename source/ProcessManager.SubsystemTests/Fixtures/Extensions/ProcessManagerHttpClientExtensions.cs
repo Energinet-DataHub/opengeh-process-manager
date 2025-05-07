550c44d2fc80d521169f35863fc35c51390e81f6
Ebbe Knudsen <xedkn@energinet.dk>
2025-05-07 08:50:01 +0200

    Add subsystem test to electrical heating

---
+++ source/ProcessManager.SubsystemTests/Fixtures/Extensions/ProcessManagerHttpClientExtensions.cs
@@ -0,0 +1,146 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Energinet.DataHub.Core.TestCommon;
+using Energinet.DataHub.ProcessManager.Abstractions.Api.Model;
+using Energinet.DataHub.ProcessManager.Abstractions.Api.Model.OrchestrationInstance;
+
+namespace Energinet.DataHub.ProcessManager.SubsystemTests.Fixtures.Extensions;
+
+public static class ProcessManagerHttpClientExtensions
+{
+    /// <summary>
+    /// Wait for an orchestration instance to be returned by the ProcessManager http client. If step inputs are provided,
+    /// then the orchestration instance must have a step instance with the given step sequence and state.
+    /// <remarks>The lookup is based on the idempotency key of the test configuration request.</remarks>
+    /// </summary>
+    /// <param name="fixture"></param>
+    /// <param name="idempotencyKey">Find an orchestration instance with the given idempotency key.</param>
+    /// <param name="orchestrationInstanceState">If provided, then the orchestration instance have the given state.</param>
+    /// <param name="stepSequence">If provided, then the orchestration instance must have a step instance with the given sequence number.</param>
+    /// <param name="stepState">If provided, then the step should be in the given state (defaults to <see cref="StepInstanceLifecycleState.Terminated"/>).</param>
+    public static async Task<(
+        bool Success,
+        OrchestrationInstanceTypedDto<TInputParameterDto>? OrchestrationInstance,
+        StepInstanceDto? StepInstance)> WaitForOrchestrationInstanceByIdempotencyKeyAsync<TInputParameterDto, TConfiguration>(
+            this ProcessManagerFixture<TConfiguration> fixture,
+            string idempotencyKey,
+            OrchestrationInstanceLifecycleState? orchestrationInstanceState = null,
+            int? stepSequence = null,
+            StepInstanceLifecycleState? stepState = null)
+                where TInputParameterDto : class, IInputParameterDto
+    {
+        if (stepState != null && stepSequence == null)
+            throw new ArgumentNullException(nameof(stepSequence), $"{nameof(stepSequence)} must be provided if {nameof(stepState)} is not null.");
+
+        OrchestrationInstanceTypedDto<TInputParameterDto>? orchestrationInstance = null;
+        StepInstanceDto? stepInstance = null;
+
+        var success = await Awaiter.TryWaitUntilConditionAsync(
+            async () =>
+            {
+                orchestrationInstance = await fixture.ProcessManagerHttpClient
+                    .GetOrchestrationInstanceByIdempotencyKeyAsync<TInputParameterDto>(
+                        new GetOrchestrationInstanceByIdempotencyKeyQuery(
+                            operatingIdentity: fixture.UserIdentity,
+                            idempotencyKey: idempotencyKey),
+                        CancellationToken.None);
+
+                if (orchestrationInstance == null)
+                    return false;
+
+                if (stepSequence != null)
+                {
+                    stepInstance = orchestrationInstance.Steps
+                        .SingleOrDefault(s => s.Sequence == stepSequence.Value);
+                }
+
+                if (orchestrationInstanceState != null && orchestrationInstance.Lifecycle.State != orchestrationInstanceState)
+                    return false;
+
+                // If step sequence is not provided, only check for orchestration instance existence
+                if (stepSequence == null)
+                    return true;
+
+                return stepInstance != null
+                    ? stepInstance.Lifecycle.State == (stepState ?? StepInstanceLifecycleState.Terminated)
+                    : throw new ArgumentException($"Step instance for step sequence {stepSequence} not found", nameof(stepSequence));
+            },
+            timeLimit: TimeSpan.FromMinutes(1),
+            delay: TimeSpan.FromSeconds(1));
+
+        return (success, orchestrationInstance, stepInstance);
+    }
+
+    /// <summary>
+    /// Wait for an orchestration instance to be returned by the ProcessManager http client. If step inputs are provided,
+    /// then the orchestration instance must have a step instance with the given step sequence and state.
+    /// </summary>
+    /// <param name="fixture"></param>
+    /// <param name="orchestrationInstanceId">Find an orchestration instance with the given id.</param>
+    /// <param name="orchestrationInstanceState">If provided, then the orchestration instance have the given state.</param>
+    /// <param name="stepSequence">If provided, then the orchestration instance must have a step instance with the given sequence number.</param>
+    /// <param name="stepState">If provided, then the step should be in the given state (defaults to <see cref="StepInstanceLifecycleState.Terminated"/>).</param>
+    public static async Task<(
+        bool Success,
+        OrchestrationInstanceTypedDto? OrchestrationInstance,
+        StepInstanceDto? StepInstance)> WaitForOrchestrationInstanceByIdAsync<TConfiguration>(
+            this ProcessManagerFixture<TConfiguration> fixture,
+            Guid orchestrationInstanceId,
+            OrchestrationInstanceLifecycleState? orchestrationInstanceState = null,
+            int? stepSequence = null,
+            StepInstanceLifecycleState? stepState = null)
+    {
+        if (stepState != null && stepSequence == null)
+            throw new ArgumentNullException(nameof(stepSequence), $"{nameof(stepSequence)} must be provided if {nameof(stepState)} is not null.");
+
+        OrchestrationInstanceTypedDto? orchestrationInstance = null;
+        StepInstanceDto? stepInstance = null;
+
+        var success = await Awaiter.TryWaitUntilConditionAsync(
+            async () =>
+            {
+                orchestrationInstance = await fixture.ProcessManagerHttpClient
+                    .GetOrchestrationInstanceByIdAsync(
+                        new GetOrchestrationInstanceByIdQuery(
+                            operatingIdentity: fixture.UserIdentity,
+                            id: orchestrationInstanceId),
+                        CancellationToken.None);
+
+                if (orchestrationInstance == null)
+                    return false;
+
+                if (stepSequence != null)
+                {
+                    stepInstance = orchestrationInstance.Steps
+                        .SingleOrDefault(s => s.Sequence == stepSequence.Value);
+                }
+
+                if (orchestrationInstanceState != null && orchestrationInstance.Lifecycle.State != orchestrationInstanceState)
+                    return false;
+
+                // If step sequence is not provided, only check for orchestration instance existence
+                if (stepSequence == null)
+                    return true;
+
+                return stepInstance != null
+                    ? stepInstance.Lifecycle.State == (stepState ?? StepInstanceLifecycleState.Terminated)
+                    : throw new ArgumentException($"Step instance for step sequence {stepSequence} not found", nameof(stepSequence));
+            },
+            timeLimit: TimeSpan.FromMinutes(1),
+            delay: TimeSpan.FromSeconds(1));
+
+        return (success, orchestrationInstance, stepInstance);
+    }
+}
---
+++ source/ProcessManager.SubsystemTests/Processes/BRS_021/ElectricalHeatingCalculation/V1/ElectricalHeatingCalculationScenario.cs
@@ -0,0 +1,108 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Energinet.DataHub.Core.TestCommon.Xunit.Attributes;
+using Energinet.DataHub.Core.TestCommon.Xunit.Orderers;
+using Energinet.DataHub.ProcessManager.Abstractions.Api.Model.OrchestrationInstance;
+using Energinet.DataHub.ProcessManager.Orchestrations.Abstractions.Processes.BRS_021.ElectricalHeatingCalculation.V1.Model;
+using Energinet.DataHub.ProcessManager.SubsystemTests.Fixtures;
+using Energinet.DataHub.ProcessManager.SubsystemTests.Fixtures.Extensions;
+using Xunit.Abstractions;
+
+namespace Energinet.DataHub.ProcessManager.SubsystemTests.Processes.BRS_021.ElectricalHeatingCalculation.V1;
+
+[TestCaseOrderer(
+    ordererTypeName: TestCaseOrdererLocation.OrdererTypeName,
+    ordererAssemblyName: TestCaseOrdererLocation.OrdererAssemblyName)]
+public class ElectricalHeatingCalculationScenario
+    : IClassFixture<ProcessManagerFixture<ElectricalHeatingCalculationScenarioState>>,
+    IAsyncLifetime
+{
+    private readonly ProcessManagerFixture<ElectricalHeatingCalculationScenarioState> _fixture;
+
+    public ElectricalHeatingCalculationScenario(
+        ProcessManagerFixture<ElectricalHeatingCalculationScenarioState> fixture,
+        ITestOutputHelper testOutputHelper)
+    {
+        _fixture = fixture;
+        _fixture.SetTestOutputHelper(testOutputHelper);
+    }
+
+    public Task InitializeAsync()
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task DisposeAsync()
+    {
+        _fixture.SetTestOutputHelper(null);
+        return Task.CompletedTask;
+    }
+
+    [SubsystemFact]
+    [ScenarioStep(1)]
+    public void Given_ValidStartElectricalHeatingCalculationCommand()
+    {
+        _fixture.TestConfiguration = new ElectricalHeatingCalculationScenarioState(
+            startCommand: new StartElectricalHeatingCalculationCommandV1(_fixture.UserIdentity));
+    }
+
+    [SubsystemFact]
+    [ScenarioStep(2)]
+    public async Task AndGiven_StartNewOrchestrationInstanceIsSent()
+    {
+        var orchestrationInstanceId = await _fixture.ProcessManagerHttpClient.StartNewOrchestrationInstanceAsync(
+            _fixture.TestConfiguration.StartCommand,
+            CancellationToken.None);
+
+        _fixture.TestConfiguration.OrchestrationInstanceId = orchestrationInstanceId;
+    }
+
+    [SubsystemFact]
+    [ScenarioStep(2)]
+    public async Task When_OrchestrationInstanceIsRunning()
+    {
+        Assert.NotNull(_fixture.TestConfiguration.OrchestrationInstance); // If orchestration instance wasn't found in earlier test, end test early.
+
+        var (success, orchestrationInstance, _) = await _fixture.WaitForOrchestrationInstanceByIdAsync(
+            orchestrationInstanceId: _fixture.TestConfiguration.OrchestrationInstanceId,
+            orchestrationInstanceState: OrchestrationInstanceLifecycleState.Running);
+
+        Assert.Multiple(
+            () => Assert.True(
+                success,
+                $"An orchestration instance for id \"{_fixture.TestConfiguration.OrchestrationInstanceId}\" should be running."),
+            () => Assert.NotNull(orchestrationInstance));
+
+        _fixture.TestConfiguration.OrchestrationInstance = orchestrationInstance;
+    }
+
+    [SubsystemFact]
+    [ScenarioStep(3)]
+    public async Task AndThen_OrchestrationInstanceIsTerminatedWithSuccess()
+    {
+        Assert.NotNull(_fixture.TestConfiguration.OrchestrationInstance); // If orchestration instance wasn't found in earlier test, end test early.
+
+        var (success, orchestrationInstance, _) = await _fixture.WaitForOrchestrationInstanceByIdAsync(
+                orchestrationInstanceId: _fixture.TestConfiguration.OrchestrationInstanceId,
+                orchestrationInstanceState: OrchestrationInstanceLifecycleState.Terminated);
+
+        _fixture.TestConfiguration.OrchestrationInstance = orchestrationInstance;
+
+        Assert.Multiple(
+            () => Assert.True(success, "The orchestration instance should be terminated"),
+            () => Assert.Equal(OrchestrationInstanceLifecycleState.Terminated, orchestrationInstance?.Lifecycle.State),
+            () => Assert.Equal(OrchestrationInstanceTerminationState.Succeeded, orchestrationInstance?.Lifecycle.TerminationState));
+    }
+}

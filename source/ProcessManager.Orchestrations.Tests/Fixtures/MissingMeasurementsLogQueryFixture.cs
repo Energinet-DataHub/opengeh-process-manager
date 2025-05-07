5b5f4ca5b81d3494b4dde48530316610a5b031aa
Alexander <122119488+energinet-ajw@users.noreply.github.com>
2025-05-07 20:01:09 +0200

    feat: Added missing measurements log query (#391)

---
+++ source/ProcessManager.Orchestrations.Tests/Fixtures/MissingMeasurementsLogQueryFixture.cs
@@ -0,0 +1,125 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Energinet.DataHub.Core.Databricks.SqlStatementExecution;
+using Energinet.DataHub.Core.FunctionApp.TestCommon.Configuration;
+using Energinet.DataHub.Core.FunctionApp.TestCommon.Databricks;
+using Energinet.DataHub.ProcessManager.Components.Extensions.Options;
+using Energinet.DataHub.ProcessManager.Orchestrations.Processes.BRS_045.Shared.Databricks.SqlStatements;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Energinet.DataHub.ProcessManager.Orchestrations.Tests.Fixtures;
+
+public class MissingMeasurementsLogQueryFixture : IAsyncLifetime
+{
+    private readonly DatabricksSchemaManager _databricksSchemaManager;
+    private readonly DatabricksQueryOptions _queryOptions;
+
+    public MissingMeasurementsLogQueryFixture()
+    {
+        var integrationTestConfiguration = new IntegrationTestConfiguration();
+
+        _databricksSchemaManager = CreateDatabricksSchemaManager(integrationTestConfiguration.DatabricksSettings);
+        _queryOptions = CreateQueryOptions(_databricksSchemaManager.SchemaName);
+
+        QueryExecutor = CreateDatabricksExecutor(integrationTestConfiguration.DatabricksSettings);
+        OrchestrationInstanceId = Guid.NewGuid();
+    }
+
+    public DatabricksSqlWarehouseQueryExecutor QueryExecutor { get; }
+
+    public Guid OrchestrationInstanceId { get; }
+
+    public async Task InitializeAsync()
+    {
+        await _databricksSchemaManager.CreateSchemaAsync();
+        await SeedDatabaseAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await _databricksSchemaManager.DropSchemaAsync();
+    }
+
+    internal MissingMeasurementsLogQuery CreateSut(Guid orchestrationInstanceId)
+    {
+        var schemaDescription = new MissingMeasurementsLogSchemaDescription(_queryOptions);
+
+        return new MissingMeasurementsLogQuery(
+            Mock.Of<ILogger>(),
+            schemaDescription,
+            orchestrationInstanceId);
+    }
+
+    private static DatabricksSchemaManager CreateDatabricksSchemaManager(
+        DatabricksSettings databricksSettings)
+    {
+        return new DatabricksSchemaManager(
+            new DataHub.Core.FunctionApp.TestCommon.Databricks.HttpClientFactory(),
+            databricksSettings,
+            schemaPrefix: nameof(MissingMeasurementsLogQueryFixture).ToLower());
+    }
+
+    private static DatabricksSqlWarehouseQueryExecutor CreateDatabricksExecutor(
+    DatabricksSettings databricksSettings)
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(new Dictionary<string, string?>
+            {
+                ["WorkspaceUrl"] = databricksSettings.WorkspaceUrl,
+                ["WarehouseId"] = databricksSettings.WarehouseId,
+                ["WorkspaceToken"] = databricksSettings.WorkspaceAccessToken,
+            })
+            .Build();
+
+        var serviceProvider = new ServiceCollection()
+            .AddDatabricksSqlStatementExecution(configuration)
+            .BuildServiceProvider();
+
+        return serviceProvider.GetService<DatabricksSqlWarehouseQueryExecutor>()!;
+    }
+
+    private static DatabricksQueryOptions CreateQueryOptions(string databaseName)
+    {
+        return new DatabricksQueryOptions
+        {
+            CatalogName = "hive_metastore",
+            DatabaseName = databaseName,
+        };
+    }
+
+    /// <summary>
+    /// Seeding the database as part of a fixture which is reused by multiple tests,
+    /// can save time in test setup when using an actual Databricks.
+    /// </summary>
+    private async Task SeedDatabaseAsync()
+    {
+        var schemaDescription = new MissingMeasurementsLogSchemaDescription(_queryOptions);
+
+        await _databricksSchemaManager.CreateTableAsync(schemaDescription.DataObjectName, schemaDescription.SchemaDefinition);
+        await _databricksSchemaManager.InsertAsync(
+            schemaDescription.DataObjectName,
+            [
+                // First metering point
+                [$"'{OrchestrationInstanceId}'", "'190000040000000001'", "'2024-01-14T23:00:00.000'"],
+                [$"'{OrchestrationInstanceId}'", "'190000040000000001'", "'2025-01-14T23:00:00.000'"],
+                // Second metering point
+                [$"'{OrchestrationInstanceId}'", "'190000040000000002'", "'2024-01-29T23:00:00.000'"],
+                [$"'{OrchestrationInstanceId}'", "'190000040000000002'", "'2025-01-29T23:00:00.000'"],
+            ]);
+    }
+}
---
+++ source/ProcessManager.Orchestrations/Processes/BRS_045/Shared/Databricks/SqlStatements/MissingMeasurementsLogQuery.cs
@@ -0,0 +1,89 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Energinet.DataHub.ProcessManager.Components.Databricks.SqlStatements;
+using Energinet.DataHub.ProcessManager.Orchestrations.Processes.BRS_045.Shared.Databricks.SqlStatements.Model;
+using Microsoft.Extensions.Logging;
+
+namespace Energinet.DataHub.ProcessManager.Orchestrations.Processes.BRS_045.Shared.Databricks.SqlStatements;
+
+internal class MissingMeasurementsLogQuery(
+    ILogger logger,
+    MissingMeasurementsLogSchemaDescription schemaDescription,
+    Guid orchestrationInstanceId) :
+        QueryBase<MissingMeasurementsLog, MissingMeasurementsLogSchemaDescription>(
+            logger,
+            schemaDescription,
+            orchestrationInstanceId)
+{
+    private readonly string _orchestrationType = "missing_measurements_log";
+
+    protected override Task<QueryResult<MissingMeasurementsLog>> CreateResultFromGroupAsync(IList<DatabricksSqlRow> groupOfRows)
+    {
+        var firstRow = groupOfRows.First();
+
+        try
+        {
+            var missingMeasurementsLogDataList = new List<MissingMeasurementsLogData>();
+
+            foreach (var row in groupOfRows)
+            {
+                missingMeasurementsLogDataList.Add(CreateMissingMeasurementsLogData(row));
+            }
+
+            var result = CreateMissingMeasurementsLog(firstRow, missingMeasurementsLogDataList);
+            return Task.FromResult(QueryResult<MissingMeasurementsLog>.Success(result));
+        }
+        catch (Exception ex)
+        {
+            Logger.LogWarning(
+                ex,
+                "Creating missing measurements log ({OrchestrationType}) failed for orchestration instance id='{OrchestrationInstanceId}'.",
+                _orchestrationType,
+                OrchestrationInstanceId);
+        }
+
+        return Task.FromResult(QueryResult<MissingMeasurementsLog>.Error());
+    }
+
+    protected override bool BelongsToSameGroup(DatabricksSqlRow currentRow, DatabricksSqlRow previousRow)
+    {
+        return previousRow.ToNonEmptyString(MissingMeasurementsLogColumnNames.MeteringPointId) == currentRow.ToNonEmptyString(MissingMeasurementsLogColumnNames.MeteringPointId);
+    }
+
+    protected override string BuildSqlQuery()
+    {
+        return $"""
+            SELECT {string.Join(", ", SchemaDescription.Columns)}
+            FROM {SchemaDescription.DatabaseName}.{SchemaDescription.DataObjectName}
+            WHERE {MissingMeasurementsLogColumnNames.OrchestrationInstanceId} = '{OrchestrationInstanceId}'
+            ORDER BY {MissingMeasurementsLogColumnNames.MeteringPointId}, {MissingMeasurementsLogColumnNames.Date}
+            """;
+    }
+
+    private static MissingMeasurementsLogData CreateMissingMeasurementsLogData(DatabricksSqlRow databricksSqlRow)
+    {
+        return new MissingMeasurementsLogData(
+            MeteringPointId: databricksSqlRow.ToNonEmptyString(MissingMeasurementsLogColumnNames.MeteringPointId),
+            Date: databricksSqlRow.ToInstant(MissingMeasurementsLogColumnNames.Date));
+    }
+
+    private MissingMeasurementsLog CreateMissingMeasurementsLog(DatabricksSqlRow databricksSqlRow, IReadOnlyCollection<MissingMeasurementsLogData> missingMeasurementsLogsData)
+    {
+        return new MissingMeasurementsLog(
+            OrchestrationType: _orchestrationType,
+            OrchestrationInstanceId: databricksSqlRow.ToGuid(MissingMeasurementsLogColumnNames.OrchestrationInstanceId),
+            MissingMeasurementsLogsData: missingMeasurementsLogsData);
+    }
+}

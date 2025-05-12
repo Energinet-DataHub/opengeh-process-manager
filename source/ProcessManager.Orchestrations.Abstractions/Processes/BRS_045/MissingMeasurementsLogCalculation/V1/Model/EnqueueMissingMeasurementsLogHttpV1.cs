d77fe50eba162369735c174d88b98ffd2daedba8
Ebbe Knudsen <xedkn@energinet.dk>
2025-05-12 13:45:06 +0200

    Update EnqueueMissingMeasurementsLogHttpV1.cs

--- source/ProcessManager.Orchestrations.Abstractions/Processes/BRS_045/MissingMeasurementsLogCalculation/V1/Model/EnqueueMissingMeasurementsLogHttpV1.cs
+++ source/ProcessManager.Orchestrations.Abstractions/Processes/BRS_045/MissingMeasurementsLogCalculation/V1/Model/EnqueueMissingMeasurementsLogHttpV1.cs
@@ -25,7 +25,7 @@ namespace Energinet.DataHub.ProcessManager.Orchestrations.Abstractions.Processes
 /// </remarks>
 /// </summary>
 /// <param name="OrchestrationInstanceId"></param>
-/// <param name="Data">The list of dates and which metering point id's that are missing data on the given date.</param>
+/// <param name="Data">The list of dates and metering point id's that are missing data on the given dates.</param>
 public record EnqueueMissingMeasurementsLogHttpV1(
     Guid OrchestrationInstanceId,
     IReadOnlyCollection<EnqueueMissingMeasurementsLogHttpV1.DateWithMeteringPointIds> Data)
@@ -36,15 +36,15 @@ public record EnqueueMissingMeasurementsLogHttpV1(
     public string Route { get; } = RouteName;
 
     /// <summary>
-    /// A representation of the message that will be sent to the given <paramref name="GridAccessProvider"/>
+    /// A representation of the message that will be sent to the given <paramref name="GridAccessProvider"/>.
     /// </summary>
-    /// <param name="GridAccessProvider">The current grid access provider for the metering points, who should receive the messages.</param>
-    /// <param name="GridArea">The grid area for the metering points and grid access provider, used determine whether the actor has delegations.</param>
-    /// <param name="Date">The date that measurements is missing for the given <paramref name="MeteringPointsIds"/>.</param>
-    /// <param name="MeteringPointsIds">The metering points that are missing measurements on the given <paramref name="Date"/>.</param>
+    /// <param name="GridAccessProvider">The current grid access provider for the metering point, who should receive the message.</param>
+    /// <param name="GridArea">The grid area for the metering point and grid access provider, used determine whether the actor has delegations.</param>
+    /// <param name="Date">The date where measurements are missing for the given <paramref name="MeteringPointId"/>.</param>
+    /// <param name="MeteringPointId">The metering point that is missing measurements on the given <paramref name="Date"/>.</param>
     public record DateWithMeteringPointIds(
         ActorNumber GridAccessProvider,
         string GridArea,
         DateTimeOffset Date,
-        IReadOnlyCollection<string> MeteringPointsIds);
+        string MeteringPointId);
 }

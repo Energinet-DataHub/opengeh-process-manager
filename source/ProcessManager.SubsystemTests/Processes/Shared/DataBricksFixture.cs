a8b6b48658f3645f1c1bea3dab3893d0064a63ae
Alexander <alexanderharrow@hotmail.com>
2025-05-19 21:02:50 +0200

    Updates

---
+++ source/ProcessManager.SubsystemTests/Processes/Shared/DataBricksFixture.cs
@@ -0,0 +1,50 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Energinet.DataHub.ProcessManager.SubsystemTests.Fixtures;
+using Microsoft.Azure.Databricks.Client;
+
+namespace Energinet.DataHub.ProcessManager.SubsystemTests.Processes.Shared;
+
+[CollectionDefinition("Databricks collection")]
+public class DataBricksCollection : ICollectionFixture<DatabricksFixture>
+{
+    // This class has no code and is never instantiated.
+    // Its purpose is to associate the fixture with the test collection.
+}
+
+public class DatabricksFixture : IAsyncLifetime
+{
+    private readonly ProcessManagerSubsystemTestConfiguration _configuration;
+
+    public DatabricksFixture()
+    {
+        _configuration = new ProcessManagerSubsystemTestConfiguration();
+    }
+
+    public async Task InitializeAsync()
+    {
+        using var client = DatabricksClient.CreateClient(
+            baseUrl: _configuration.ProcessManagerDatabricksWorkspaceUrl,
+            token: _configuration.ProcessManagerDatabricksToken);
+
+        await client.SQL.Warehouse.Start(id: _configuration.ProcessManagerDatabricksSqlWarehouseId);
+    }
+
+    public Task DisposeAsync()
+    {
+        // Cleanup logic if needed
+        return Task.CompletedTask;
+    }
+}

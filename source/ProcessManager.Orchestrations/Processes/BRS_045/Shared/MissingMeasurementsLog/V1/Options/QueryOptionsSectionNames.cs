950452687af89d8506406594f5afe558ea0e566d
Alexander <alexanderharrow@hotmail.com>
2025-05-20 14:10:19 +0200

    Update QueryOptionsSectionNames.cs

--- source/ProcessManager.Orchestrations/Processes/BRS_045/Shared/MissingMeasurementsLog/V1/Options/QueryOptionsSectionNames.cs
+++ source/ProcessManager.Orchestrations/Processes/BRS_045/Shared/MissingMeasurementsLog/V1/Options/QueryOptionsSectionNames.cs
@@ -17,7 +17,7 @@ namespace Energinet.DataHub.ProcessManager.Orchestrations.Processes.BRS_045.Shar
 public static class QueryOptionsSectionNames
 {
    /// <summary>
-    /// Used to name a query to avoid naming conflicts with other queries.
+    /// Used to name a query to avoid naming conflicts with other  queries.
     /// </summary>
     public const string MissingMeasurementsLogQuery = "MissingMeasurementsLogQuery";
 }

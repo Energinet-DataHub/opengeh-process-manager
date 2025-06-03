13ef376f05558458a6dad1b2e1a0cd5223d0e5f8
Mads Magnus Due <xmadu@energinet.dk>
2025-06-03 13:27:20 +0200

    feat: added brs024 metering point validation rule (#439)
    Co-authored-by: RasmusGraabaek <rasmus.graabaek@hotmail.com>

---
+++ source/ProcessManager.Orchestrations.Tests/Unit/Processes/BRS_024/V1/RequestYearlyMeasurementsInputV1Builder.cs
@@ -0,0 +1,43 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Energinet.DataHub.ProcessManager.Abstractions.Core.ValueObjects;
+using Energinet.DataHub.ProcessManager.Components.Abstractions.ValueObjects;
+using Energinet.DataHub.ProcessManager.Orchestrations.Abstractions.Processes.BRS_024.V1.Model;
+
+namespace Energinet.DataHub.ProcessManager.Orchestrations.Tests.Unit.Processes.BRS_024.V1;
+
+public class RequestYearlyMeasurementsInputV1Builder
+{
+    private const string ActorMessageId = "MessageId";
+    private const string TransactionId = "TransactionId";
+    private const string ActorNumber = "1234567890123";
+    private const string MeteringPointId = "MeteringPointId";
+    private const string ReceivedAt = "2024-12-31T23:00Z";
+
+    private readonly string _actorRole = ActorRole.GridAccessProvider.Name;
+    private readonly string _businessReason = BusinessReason.PeriodicMetering.Name;
+
+    public RequestYearlyMeasurementsInputV1 Build()
+    {
+        return new RequestYearlyMeasurementsInputV1(
+            ActorMessageId: ActorMessageId,
+            TransactionId: TransactionId,
+            ActorNumber: ActorNumber,
+            ActorRole: _actorRole,
+            BusinessReason: _businessReason,
+            ReceivedAt: ReceivedAt,
+            MeteringPointId: MeteringPointId);
+    }
+}
---
+++ source/ProcessManager.Orchestrations/Processes/BRS_024/V1/BusinessValidation/MeteringPointTypeValidationRule.cs
@@ -0,0 +1,51 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Energinet.DataHub.ProcessManager.Components.Abstractions.ValueObjects;
+using Energinet.DataHub.ProcessManager.Components.BusinessValidation;
+using Energinet.DataHub.ProcessManager.Orchestrations.Processes.BRS_024.V1.Model;
+
+namespace Energinet.DataHub.ProcessManager.Orchestrations.Processes.BRS_024.V1.BusinessValidation;
+
+public class MeteringPointTypeValidationRule
+    : IBusinessValidationRule<RequestYearlyMeasurementsBusinessValidatedDto>
+{
+    public static IList<ValidationError> WrongMeteringPointTypeError => [new(
+        Message: "I forbindelse med anmodning om årssum kan der kun anmodes om data for forbrug og produktion/When requesting yearly amount then it is only possible to request for production and consumption",
+        ErrorCode: "D18")];
+
+    private static IList<ValidationError> NoError => [];
+
+    private static IReadOnlyCollection<MeteringPointType> AllowedMeteringPointTypes => new[]
+    {
+        MeteringPointType.Production,
+        MeteringPointType.Consumption,
+    };
+
+    public Task<IList<ValidationError>> ValidateAsync(RequestYearlyMeasurementsBusinessValidatedDto subject)
+    {
+        if (subject.MeteringPointMasterData is null)
+        {
+            return Task.FromResult(NoError);
+        }
+
+        var masterDataMeteringPointType = subject.MeteringPointMasterData.MeteringPointType;
+        if (!AllowedMeteringPointTypes.Contains(masterDataMeteringPointType))
+        {
+            return Task.FromResult(WrongMeteringPointTypeError);
+        }
+
+        return Task.FromResult(NoError);
+    }
+}

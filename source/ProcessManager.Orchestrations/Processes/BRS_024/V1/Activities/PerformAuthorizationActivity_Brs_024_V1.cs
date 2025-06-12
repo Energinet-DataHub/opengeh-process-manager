2b030e697da17e3eac68110cb86597028858e43f
Mads Magnus Due <madsmagnusdue@hotmail.com>
2025-06-12 15:18:47 +0200

    feat: get authorized periods from MarkPart for Brs024+025

---
+++ source/ProcessManager.Orchestrations/Processes/BRS_024/V1/Activities/PerformAuthorizationActivity_Brs_024_V1.cs
@@ -0,0 +1,70 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Energinet.DataHub.ProcessManager.Abstractions.Core.ValueObjects;
+using Energinet.DataHub.ProcessManager.Components.Authorization;
+using Energinet.DataHub.ProcessManager.Components.Authorization.Model;
+using Energinet.DataHub.ProcessManager.Core.Application.Orchestration;
+using Energinet.DataHub.ProcessManager.Core.Domain.OrchestrationInstance;
+using Energinet.DataHub.ProcessManager.Orchestrations.Abstractions.Processes.BRS_024.V1.Model;
+using Energinet.DataHub.ProcessManager.Orchestrations.Processes.BRS_024.V1.Orchestration.Steps;
+using Microsoft.Azure.Functions.Worker;
+using MeteringPointId = Energinet.DataHub.ProcessManager.Components.Authorization.Model.MeteringPointId;
+
+namespace Energinet.DataHub.ProcessManager.Orchestrations.Processes.BRS_024.V1.Activities;
+
+public class PerformAuthorizationActivity_Brs_024_V1(
+    IOrchestrationInstanceProgressRepository repository,
+    IAuthorization authorization)
+{
+    private readonly IOrchestrationInstanceProgressRepository _repository = repository;
+    private readonly IAuthorization _authorization = authorization;
+
+    [Function(nameof(PerformAuthorizationActivity_Brs_024_V1))]
+    public async Task<ActivityOutput> Run(
+        [ActivityTrigger] ActivityInput input)
+    {
+        var orchestrationInstance = await _repository
+            .GetAsync(input.OrchestrationInstanceId)
+            .ConfigureAwait(false);
+
+        var orchestrationInstanceInput = orchestrationInstance.ParameterValue.AsType<RequestYearlyMeasurementsInputV1>();
+
+        var validatedPeriods = await _authorization
+            .GetAuthorizedPeriodsAsync(
+                actorNumber: ActorNumber.Create(orchestrationInstanceInput.ActorNumber),
+                actorRole: ActorRole.FromName(orchestrationInstanceInput.ActorRole),
+                meteringPointId: new MeteringPointId(orchestrationInstanceInput.MeteringPointId),
+                requestedPeriod: new RequestedPeriod())
+            .ConfigureAwait(false);
+
+        var isAuthorization = validatedPeriods.Count == 0;
+
+        if (!isAuthorization)
+        {
+            var step = orchestrationInstance.GetStep(input.StepSequence);
+            step.CustomState.SetFromInstance(new AuthorizationStep.CustomState(
+                IsValid: false));
+            await _repository.UnitOfWork.CommitAsync().ConfigureAwait(false);
+        }
+    }
+
+    public record ActivityInput(
+        OrchestrationInstanceId OrchestrationInstanceId,
+        int StepSequence);
+
+    public record ActivityOutput(
+        bool IsAuthorized,
+        IReadOnlyCollection<AuthorizedPeriod> ValidatedPeriod);
+}
---
+++ source/ProcessManager.Orchestrations/Processes/BRS_024/V1/Orchestration/Steps/AuthorizationStep.cs
@@ -0,0 +1,52 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Energinet.DataHub.ProcessManager.Components.BusinessValidation;
+using Energinet.DataHub.ProcessManager.Core.Domain.OrchestrationInstance;
+using Energinet.DataHub.ProcessManager.Orchestrations.Processes.BRS_024.V1.Activities;
+using Energinet.DataHub.ProcessManager.Shared.Processes.Activities;
+using Microsoft.DurableTask;
+
+namespace Energinet.DataHub.ProcessManager.Orchestrations.Processes.BRS_024.V1.Orchestration.Steps;
+
+internal class AuthorizationStep(
+    TaskOrchestrationContext context,
+    TaskRetryOptions defaultRetryOptions,
+    OrchestrationInstanceId instanceId)
+        : StepExecutor<PerformBusinessValidationActivity_Brs_024_V1.ActivityOutput>(
+        context,
+        defaultRetryOptions,
+        instanceId)
+{
+    internal const string StepDescription = "Authorization";
+    internal const int StepSequence = 2;
+
+    protected override int StepSequenceNumber => StepSequence;
+
+    protected override async Task<StepOutput> OnExecuteAsync()
+    {
+        var authorizationResult = await Context.CallActivityAsync<PerformAuthorizationActivity_Brs_024_V1.ActivityOutput>(
+            nameof(PerformAuthorizationActivity_Brs_024_V1),
+            new PerformAuthorizationActivity_Brs_024_V1.ActivityInput(
+                InstanceId,
+                StepSequence),
+            DefaultRetryOptions);
+
+        var asyncAuthorizationTerminationState = authorizationResult.IsAuthorized
+            ? StepInstanceTerminationState.Succeeded
+            : StepInstanceTerminationState.Failed;
+
+        return new StepOutput(asyncAuthorizationTerminationState, authorizationResult);
+    }
+}
